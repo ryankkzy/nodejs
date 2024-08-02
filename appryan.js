35a34a780ab61aa4234732bd69c79340030815bd
ryankkzy <ryan37370308@gmail.com>
2024-08-02 11:16:39 -0300

    adicionando sominha

--- appryan.js
+++ appryan.js
@@ -1,1 +1,21 @@
-console.log('oi');
+import express from 'express'
+const servidor = express()
+servidor.use(express.json())
+
+servidor.get('/calculadora/soma/:n1/:n2', (req,resp) => {
+    let n1 = Number(req.params.n1)
+    let n2 = Number(req.params.n2)
+    let soma = n1 + n2;
+
+    resp.send({
+        entradas: {
+            numero1: n1,
+            numero2: n2
+        },
+        soma: soma
+    })
+})
+
+servidor.listen(
+    5001,
+     () => console.log('API SUBIU BOLADO NA PORTA 5001'))
