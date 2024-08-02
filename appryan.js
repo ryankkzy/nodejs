6e178ae4823664c55246c4939d8ad7958ba86d95
bragapjl <surtinsads2@gmail.com>
2024-08-02 11:30:53 -0300

    divisao

--- appryan.js
+++ appryan.js
@@ -32,6 +32,23 @@ servidor.get('/calcula/subtra/:n1/:n2', (req,resp) => {
     })
 })
 
+servidor.get('/calcula/div/:n1/:n2', (req,resp) => {
+
+    let n1 = Number(req.params.n1)
+    let n2 = Number(req.params.n2)
+
+    let div = n1 / n2
+
+    resp.send({
+
+        entradas: {
+            numero1: n1,
+            numero2: n2
+        },
+        subtracao: subtracao
+    })
+})
+
 servidor.listen(
     5001,
      () => console.log('API SUBIU BOLADO NA PORTA 5001'))
