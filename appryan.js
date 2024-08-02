ef1750433d93191a486a99399f21d74c5d95c6b1
bragapjl <surtinsads2@gmail.com>
2024-08-02 11:28:35 -0300

    subtra

--- appryan.js
+++ appryan.js
@@ -16,7 +16,21 @@ servidor.get('/calculadora/soma/:n1/:n2', (req,resp) => {
     })
 })
 
-servidor.get('/calcula/div/:n1/:n2')
+servidor.get('/calcula/subtra/:n1/:n2', (req,resp) => {
+
+    let n1 = Number(req.params.n1)
+    let n2 = Number(req.params.n2)
+
+    let subtracao = n1 - n2
+
+    resp.send({
+        entradas: {
+            numero1: n1,
+            numero2: n2
+        },
+        subtracao: subtracao
+    })
+})
 
 servidor.listen(
     5001,
